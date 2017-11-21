0ecd81153a30fd281d3f8fae3100ce0184d4f0ed
Majid Mokhtari <majidmokht@gmail.com>
2017-11-20 16:23:26 -0800

    Use configure function inside store instead

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import AppContainer from './containers/AppContainer'
 import './App.css'
-
-//middleware
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux'
-import thunkMiddleware from 'redux-thunk';
-import {rootReducer} from './reducers'
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
-const store = createStoreWithMiddleware(rootReducer);
+import configureStore from './store/store';
+
+const store = configureStore();
 
 class App extends Component {
   render() {
